02749ec11d7062493d169b12dfca1e0b9748229b
Scott <scottgangemi@gmail.com>
2016-12-29 09:28:44 +1100

    complete implmentation

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import {render} from 'react-dom';
 var marked=require('marked');
-console.log(marked('I am using __markdown__.'));
 
 class App extends React.Component {
 	constructor(props, context) {
 	    super(props, context);
+	    var string = 'Header 1\n =======\n Header 2\n -----------\n ### Header 3 \n Paragraphs are separated by a blank line. \n\n Leave 2 spaces at the end of a line to do a line break \n\n Text attributes *italic*, **bold**, `monospace`, ~~strikethrough~~ . \n\n Numbered list:  1. apple  2. oranges  3. pears \n\n *[Scott Gangemi](http://www.scottgangemi.com)*';
+		var markup = marked(string, {sanitize: true});
 	    this.state = {
-	      input: ''
+	      input: markup
 	    };
 	  };
 
 	  render() {
 	    return  (
-	    <div>
-		    <UserInput handleChange={this.handleChange.bind(this)}/>
-		    <UserOutput markup={this.state.input}/>
-	    </div>
+	    	<div className="container">
+			    <div className="row">
+				    <UserInput handleChange={this.handleChange.bind(this)}/>
+				    <UserOutput markup={this.state.input}/>
+			    </div>
+	    	</div>
 	    );
 	  }
 
+
+
 	  handleChange(e) {
-	  	var markup = marked(e.target.value);
-	  	console.log(markup);
+	  	var markup = marked(e.target.value, {sanitize: true});
 	  	this.setState({
 	  		input: markup
 		});
@@ -33,7 +37,7 @@ class UserInput extends React.Component {
 	constructor(props, context) {
 	    super(props, context);
 	    this.state = {
-	      inputValue: ''
+	      inputValue: 'Header 1\n =======\n Header 2\n -----------\n ### Header 3 \n Paragraphs are separated by a blank line. \n\n Leave 2 spaces at the end of a line to do a line break \n\n Text attributes *italic*, **bold**, `monospace`, ~~strikethrough~~ . \n\n Numbered list:  1. apple  2. oranges  3. pears \n\n *[Scott Gangemi](http://www.scottgangemi.com)*'
 	    };
 	  };
 
@@ -44,6 +48,7 @@ class UserInput extends React.Component {
 
 	handleTyping(e) {
 		//setting the state rerenders the react component. when we're typing into the box, react is almost instantly rerendering that component for us, our text isn't being rendered by the HTML.
+
 		this.setState({
       		inputValue: e.target.value
     	});
@@ -52,11 +57,15 @@ class UserInput extends React.Component {
 
 	render () {
 		return (
-			<div>
-				<h2> {this.props.name} </h2>
-				<input className="user-input" value={this.state.inputValue} onChange={this.handleTyping.bind(this)}/>
+			<div className="col-md-6">
+				<textarea id="user-input" value={this.state.inputValue} onChange={this.handleTyping.bind(this)}/>
 			</div>
 		);
+
+		//document.getElementById("user-input").onchange();
+		var element = document.getElementById('just_an_example');
+		var event = new Event('change');
+		element.dispatchEvent(event);
 	}
 }
 
@@ -65,12 +74,14 @@ class UserOutput extends React.Component {
 		markup: React.PropTypes.string,
 	}
 
+	createMarkup() {
+		return {__html: this.props.markup};
+	}
+
 	render() {
 		return (
-			<div className="user-output">
-				<span>
-					{this.props.markup}
-				</span>
+			<div className='col-md-6'>
+				<div dangerouslySetInnerHTML={this.createMarkup()} />
 			</div>
 		);
 	}
